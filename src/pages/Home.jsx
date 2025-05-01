3d08f3617ad979608da61460e50fdee998c12abc
asif7480 <a.a.khan7480@gmail.com>
2025-05-01 12:36:29 +0500

    resolve elements order issue after updating it.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ function Home() {
   const [products, setProducts] = useState([])
 
   const fetchProducts = async() => {
-    const { data, error } = await supabase.from("products").select()
+    const { data, error } = await supabase.from("products").select().order("id", { ascending: true })
 
     if(error){
         setFetchError("Error in fetching data.")
